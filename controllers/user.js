c437ab8bf89577afc9f7cc1eedd45530216bd52c
Roberson Garcia <roberson.gar12@gmail.com>
2023-08-20 01:34:58 -0400

    we keep going

--- controllers/user.js
+++ controllers/user.js
@@ -20,6 +20,16 @@ const usersPost = async (req = request, res = response) => {
 
     const { name, email, password, role } = req.body
 
+    const emailExist = await User.findOne({
+        email
+    })
+
+    if(emailExist){
+        return res.status(400).json({
+            msg: 'the email already exist in the db'
+        })
+    }
+
     const user = new User({ name, email, password:'', role })
     
     const salt = bcryptjs.genSaltSync(10)
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,22 @@
 const {Router} = require('express');
 const { usersGet, usersPost, usersPut, usersDelete } = require('../controllers/user');
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validateFields');
 
 const router = Router();
 
 router.get('/', usersGet)
-router.post('/', usersPost)
+router.post('/', [check('email', 'email is not valid').isEmail(), check('name', 'name is required').not().isEmpty(),check('password', 'password is required').isLength({
+    min: 6
+}), check('role', 'its not a valid role').isIn(['ADMIN_ROLE','USER_ROLE']), validateFields ] ,usersPost)
 router.put('/:id', usersPut)
 router.delete('/', usersDelete)
 
 module.exports = router;
+
+// you can make custom checks like
+
+// check('role').custom( async (role = '') => {
+//     custom validate something
+//     if go wrong throw new Error
+// })
